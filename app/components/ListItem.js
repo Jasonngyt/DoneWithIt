1213b02c14d71d431683d13da0c242d59694bc1f
jasonngyt <jasonngyt@gmail.com>
2020-08-24 21:00:43 +0800

    Handle Selections

--- app/components/ListItem.js
+++ app/components/ListItem.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, View, Image, TouchableHighlight } from 'react-native';
 import AppText from './AppText';
 
 import colors from '../config/colors';
 
-export default function ListItem({ title, subTitle, image }) {
+export default function ListItem({ title, subTitle, image, onPress }) {
   return (
-    <View style={styles.container}>
-      <Image style={styles.image} source={image} />
-      <View>
-        <AppText style={styles.title}>{title}</AppText>
-        <AppText style={styles.subTitle}>{subTitle}</AppText>
+    <TouchableHighlight underlayColor={colors.light} onPress={onPress}>
+      <View style={styles.container}>
+        <Image style={styles.image} source={image} />
+        <View>
+          <AppText style={styles.title}>{title}</AppText>
+          <AppText style={styles.subTitle}>{subTitle}</AppText>
+        </View>
       </View>
-    </View>
+    </TouchableHighlight>
   );
 }
 
