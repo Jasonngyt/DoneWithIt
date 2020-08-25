40b1887f92a37be2562f8b3b87c68ed378dc1a19
jasonngyt <jasonngyt@gmail.com>
2020-08-25 15:29:17 +0800

    Text Input

--- App.js
+++ App.js
@@ -1,18 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
-import ViewImageScreen from './app/screens/ViewImageScreen';
-import MessagesScreen from './app/screens/MessagesScreen';
 import Screen from './app/components/Screen';
-import Icon from './app/components/Icon';
-import ListItem from './app/components/ListItem';
-import AccountScreen from './app/screens/AccountScreen';
-import ListingsScreen from './app/screens/ListingsScreen';
+import { TextInput, Text } from 'react-native';
+import AppTextInput from './app/components/AppTextInput';
 
 export default function App() {
+  const [firstName, setFirstName] = useState('');
+
   return (
     <Screen>
-      <ListingsScreen />
+      <AppTextInput placeholder='Username' icon='email' />
     </Screen>
   );
 }
