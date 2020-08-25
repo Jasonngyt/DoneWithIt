ad5718279a4ec1632c6dd39b91dc229651fcb7c9
jasonngyt <jasonngyt@gmail.com>
2020-08-25 20:00:49 +0800

    Add Picker Item

--- App.js
+++ App.js
@@ -2,14 +2,28 @@ import React, { useState } from 'react';
 
 import Screen from './app/components/Screen';
 import { TextInput, Text, Switch } from 'react-native';
+import AppPicker from './app/components/AppPicker';
 import AppTextInput from './app/components/AppTextInput';
 
+const categories = [
+  { label: 'Furniture', value: 1 },
+  { label: 'Clothing', value: 2 },
+  { label: 'Cameras', value: 3 },
+];
+
 export default function App() {
-  const [isNew, setIsNew] = useState(false);
+  const [category, setCategory] = useState(categories[0]);
 
   return (
     <Screen>
-      <Switch value={isNew} onValueChange={(newValue) => setIsNew(newValue)} />
+      <AppPicker
+        selectedItem={category}
+        onSelectItem={(item) => setCategory(item)}
+        items={categories}
+        icon='apps'
+        placeholder='Category'
+      />
+      <AppTextInput icon='email' placeholder='Email' />
     </Screen>
   );
 }
