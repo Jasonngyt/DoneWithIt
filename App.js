8d219c6cc5833719d75f11ccba7b8847f0934d72
jasonngyt <jasonngyt@gmail.com>
2020-09-08 23:33:45 +0800

    Building ImageInputLayout

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import * as ImagePicker from 'expo-image-picker';
-import * as Permissions from 'expo-permissions';
-
+import Button from './app/components/Button';
 import Screen from './app/components/Screen';
+import { Image } from 'react-native';
+import ImageInput from './app/components/ImageInput';
 
 export default function App() {
+  const [imageUri, setImageUri] = useState();
+
   const requestPermission = async () => {
-    Permissions.asAsync(Permissions.CAMERA_ROLL);
     const { granted } = await ImagePicker.requestCameraRollPermissionsAsync();
     if (!granted) alert('You need to enable permission to access the library');
   };
@@ -15,5 +17,20 @@ export default function App() {
     requestPermission();
   }, []);
 
-  return <Screen />;
+  const selectImage = async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync();
+      if (result.cancelled) setImageUri(result.uri);
+    } catch (error) {
+      console.log('Error reading an image', error);
+    }
+  };
+
+  return (
+    <Screen>
+      <Button title='Select Image' onPress={selectImage} />
+      <Image source={{ uri: imageUri }} style={{ width: 200, height: 200 }} />
+      <ImageInput imageUri={imageUri} />
+    </Screen>
+  );
 }
