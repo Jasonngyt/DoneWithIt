a13961fffc7e91406a00534812948ca17be13a16
jasonngyt <jasonngyt@gmail.com>
2020-09-10 18:10:26 +0800

    Navigation Between Screens

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, Button } from 'react-native';
 import Screen from './app/components/Screen';
 import { createStackNavigator } from '@react-navigation/stack';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
 
-const Tweets = () => (
+const Link = () => {
+  const navigation = useNavigation();
+
+  return (
+    <Button title='Click' onPress={() => navigation.navigate('TweetDetails')} />
+  );
+};
+
+const Tweets = ({ navigation }) => (
   <Screen>
     <Text>Tweets</Text>
+    <Link />
   </Screen>
 );
 
