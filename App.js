1d0d4cd58adc995a2243249ca390875cfd55c156
jasonngyt <jasonngyt@gmail.com>
2020-09-10 23:04:28 +0800

    Setting Screen Titles

--- App.js
+++ App.js
@@ -33,7 +33,11 @@ const Stack = createStackNavigator();
 const StackNagivator = () => (
   <Stack.Navigator>
     <Stack.Screen name='Tweets' component={Tweets} />
-    <Stack.Screen name='TweetDetails' component={TweetDetails} />
+    <Stack.Screen
+      name='TweetDetails'
+      component={TweetDetails}
+      options={({ route }) => ({ title: route.params.id })}
+    />
   </Stack.Navigator>
 );
 
