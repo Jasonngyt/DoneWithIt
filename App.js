1152738c7a256e1950b56f08e314cf8dec32c110
jasonngyt <jasonngyt@gmail.com>
2020-09-10 17:48:38 +0800

    Stack Navigator

--- App.js
+++ App.js
@@ -1,8 +1,34 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import { Text } from 'react-native';
 import Screen from './app/components/Screen';
-import ImageInputList from './app/components/ImageInputList';
-import ListingEditScreen from './app/screens/ListingEditScreen';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+const Tweets = () => (
+  <Screen>
+    <Text>Tweets</Text>
+  </Screen>
+);
+
+const TweetDetails = () => (
+  <Screen>
+    <Text>Tweet Details</Text>
+  </Screen>
+);
+
+const Stack = createStackNavigator();
+
+const StackNagivator = () => (
+  <Stack.Navigator>
+    <Stack.Screen name='Tweets' component={Tweets} />
+    <Stack.Screen name='TweetDetails' component={TweetDetails} />
+  </Stack.Navigator>
+);
 
 export default function App() {
-  return <ListingEditScreen />;
+  return (
+    <NavigationContainer>
+      <StackNagivator />
+    </NavigationContainer>
+  );
 }
