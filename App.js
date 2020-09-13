b8bb7b6a684c3ddd44fcd14f844e901474a3c343
jasonngyt <jasonngyt@gmail.com>
2020-09-13 09:57:03 +0800

    Navigation Theme

--- App.js
+++ App.js
@@ -1,15 +1,12 @@
 import React from 'react';
-import { Text, Button } from 'react-native';
-import Screen from './app/components/Screen';
-import { createStackNavigator } from '@react-navigation/stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer, useNavigation } from '@react-navigation/native';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
+
 import AuthNavigator from './app/navigation/AuthNavigation';
+import NavigationTheme from './app/navigation/NavigationTheme';
 
 export default function App() {
   return (
-    <NavigationContainer>
+    <NavigationContainer theme={NavigationTheme}>
       <AuthNavigator />
     </NavigationContainer>
   );
