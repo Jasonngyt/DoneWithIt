211e64e858d083087e14f8b2f2afed9346b204dd
jasonngyt <jasonngyt@gmail.com>
2020-09-16 18:12:27 +0800

    Building Account Navigator

--- app/navigation/AppNavigator.js
+++ app/navigation/AppNavigator.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import AccountScreen from '../screens/AccountScreen';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import FeedNavigator from './FeedNavigator';
 import ListingsScreen from '../screens/ListingsScreen';
 import ListingEditScreen from '../screens/ListingEditScreen';
 
@@ -8,7 +9,7 @@ const Tab = createBottomTabNavigator();
 
 const AppNavigator = () => (
   <Tab.Navigator>
-    <Tab.Screen name='Listings' component={ListingsScreen} />
+    <Tab.Screen name='Feed' component={FeedNavigator} />
     <Tab.Screen name='ListingEdit' component={ListingEditScreen} />
     <Tab.Screen name='Account' component={AccountScreen} />
   </Tab.Navigator>
